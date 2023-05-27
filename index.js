aa45981b6be271953275e89db2351e8fb86e9ed5
jooaoVSilva <joao.silva850@sptech.school>
2023-05-26 21:42:36 -0300

    validações das inputs

---
+++ index.js
@@ -0,0 +1,160 @@
+var acertos = 0
+
+function verificar_1() {
+    var resposta_usuario = id_cpu.value
+
+    if (resposta_usuario.toUpperCase() != "CPU") {
+        alert("Errou")
+    } else {
+
+    }
+}
+
+function verificar_2() {
+    var resposta_usuario = id_ula.value
+
+    if (resposta_usuario.toUpperCase() != "ULA") {
+        alert("Errou")
+    } else {
+
+    }
+}
+
+function verificar_3() {
+    var resposta_usuario = id_registradores.value
+
+    if (resposta_usuario.toUpperCase() != "REGISTRADORES") {
+        alert("Errou")
+    } else {
+
+    }
+}
+
+function verificar_4() {
+    var resposta_usuario = id_ram.value
+
+    if (resposta_usuario.toUpperCase() != "RAM") {
+        alert("Errou")
+    } else {
+
+    }
+}
+
+function verificar_5() {
+    var resposta_usuario = id_rom.value
+
+    if (resposta_usuario.toUpperCase() != "ROM") {
+        alert("Errou")
+    } else {
+
+    }
+}
+
+function verificar_6() {
+    var resposta_usuario = id_eprom.value
+
+    if (resposta_usuario.toUpperCase() != "EPROM") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_7() {
+    var resposta_usuario = id_flash.value
+
+    if (resposta_usuario.toUpperCase() != "FLASH") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_8() {
+    var resposta_usuario = id_memoria_massa.value
+
+    if (resposta_usuario.toUpperCase() != "MEMORIA-MASSA") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_9() {
+    var resposta_usuario = id_dma.value
+
+    if (resposta_usuario.toUpperCase() != "DMA") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_10() {
+    var resposta_usuario = id_chip_select.value
+
+    if (resposta_usuario.toUpperCase() != "CHIP-SELECT") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_11() {
+    var resposta_usuario = id_i5.value
+
+    if (resposta_usuario.toUpperCase() != "I5") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_12() {
+    var resposta_usuario = id_data_bus.value
+
+    if (resposta_usuario.toUpperCase() != "DATA-BUS") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_13() {
+    var resposta_usuario = id_i7.value
+
+    if (resposta_usuario.toUpperCase() != "I7") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_14() {
+    var resposta_usuario = id_dual_core.value
+
+    if (resposta_usuario.toUpperCase() != "DUAL-CORE" ) {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_15() {
+    var resposta_usuario = id_quad_core.value
+
+    if (resposta_usuario.toUpperCase() != "QUAD-CORE") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_16() {
+    var resposta_usuario = id_threads.value
+
+    if (resposta_usuario.toUpperCase() != "THREADS") {
+        alert("Errou")
+    } else {
+
+    }
+}
+function verificar_17() {
+    var resposta_usuario = id_cache.value
+
+    if (resposta_usuario.toUpperCase() != "CACHE") {
+        alert("Errou")
+    } else {
+
+    }
+}
